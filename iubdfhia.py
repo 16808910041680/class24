b8c11c3901e763c3a79cac8f8f169e3738f5d0b3
16808910041680 <forforfirefire@gmail.com>
2025-08-03 11:04:34 +1000

    giuo.

---
+++ iubdfhia.py
@@ -0,0 +1,16 @@
+class cars:
+    car1 = "Toyota"
+    car2 = "Honda"
+    car3 = "Ford"
+    def __init__(self):
+        self.car1 = "Toyota"
+        self.car2 = "Honda"
+        self.car3 = "Ford"
+obj = cars()
+print("Cars available:")
+print(obj.car1)
+print(obj.car2)
+print(obj.car3)
+print("Car 1:", cars.car1, "50000")
+print("Car 2:", cars.car2, "60000")
+print("Car 3:", cars.car3, "59487") 
